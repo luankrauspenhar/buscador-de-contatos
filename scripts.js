8f62abd808b41f5e0a127485c7ab9c245c94d923
luankrauspenhar <luankrauspenhar@gmail.com>
2024-10-16 15:42:43 -0300

    buscador de contatos com forof

--- scripts.js
+++ scripts.js
@@ -1,6 +1,17 @@
-const searchInput = document.getElementById("searchInput");
-const searchButton = document.getElementById("searchButton");
-const contactsList = document.getElementById("contactsList");
+const input = document.querySelector('input')
+const p = document.querySelector('p')
+
+function search() {
+    for (const contact of contacts) {
+        if(input.value.toLowerCase() === contact.name.toLowerCase()) {
+            p.innerHTML = `Contato encontrado: ${contact.name} tel:${contact.number}`
+
+            break
+        } else {
+            p.innerHTML = "Contato não encontrado"
+        }
+    }
+}
 
 const contacts = [
     {name:"Luan", number:"(51)99358-6706" }, 
@@ -8,24 +19,3 @@ const contacts = [
     {name:"Eliane", number:"(51)98922-0966"} ,
     {name:"Lucas", number:"(51)99283-5062"}
 ]
-
-function searchContact() {
-    const nameSearched = searchInput.value.toLowerCase();
-    contactsList.innerHTML = "";
-
-    let itFound = false;
-
-    for (let i = 0; i < contacts.length; i++) {
-        if (contacts[i].name.toLowerCase().includes(nameSearched)) {
-            const p = document.createElement("p");
-            p.textContent = `${contacts[i].name} - ${contacts[i].number}`;
-            contactsList.appendChild(p);
-            itFound = true;
-          }
-    }
-
-    if (!itFound) {
-        contactsList.innerHTML = "<p>Contato não encontrado.</p>";
-    }
-}
-searchButton.addEventListener("click", searchContact);
